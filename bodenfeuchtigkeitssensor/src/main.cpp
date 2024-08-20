0af7c159218af68dfa98e1cf6ba2180711a4838f
erikscheer <erikscheer@icloud.com>
2024-08-20 16:45:52 +0200

    optimizing watering algorithm

--- bodenfeuchtigkeitssensor/src/main.cpp
+++ bodenfeuchtigkeitssensor/src/main.cpp
@@ -3,6 +3,11 @@
 #define SENSOR_PIN 33
 #define MOTOR_PIN 27
 
+
+int waitingUntilRemeasure = 10000;
+int wateringTime = 1000;
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(SENSOR_PIN, INPUT);
@@ -15,6 +20,9 @@ void loop() {
   Serial.println(analogRead(SENSOR_PIN));
   if (analogRead(SENSOR_PIN) >= 2000){
     digitalWrite(MOTOR_PIN, HIGH);
+    delay(wateringTime)
+    digitalWrite(MOTOR_PIN, LOW)
+    delay(waitingUntilRemeasure)
   }
   else{
     digitalWrite(MOTOR_PIN, LOW);
